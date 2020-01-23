47d1f8738e5a1b5c94cdc8b211c58c459da3a031
Peter Bukuras <peterbukuras@gmail.com>
2020-01-23 18:11:12 -0500

    Active Storage Gem and Upload Pictures

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -41,7 +41,7 @@ before_action :find_pic, only: [:show, :edit, :update, :destroy]
   private
 
   def pic_params
-    params.require(:pic).permit(:title, :description)
+    params.require(:pic).permit(:title, :description, :image)
   end
 
   def find_pic
